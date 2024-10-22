ee96bade9f8d0214fec77ab1f7389d32797665a3
Potter14324 <132168110+Potter14324@users.noreply.github.com>
2024-10-22 19:11:03 +0200

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -5,11 +5,19 @@
 // @description  Enhance Vanis.io with custom features
 // @author       Your Name
 // @match        https://vanis.io/*
-// @require      https://raw.githack.com/Potter14324/github.io-Vanis-Extension/main/userscript.js
 // @grant        GM_addStyle
 // @run-at document-end
 // ==/UserScript==
 
+const observer = new MutationObserver(() => {
+    // Your code to change the DOM goes here
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
+
+setTimeout(() => {
+    // Your code to change the DOM goes here
+}, 1000); // 
 
 (function() {
     'use strict';
