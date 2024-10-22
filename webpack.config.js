ab4c0ab4dbe1cb86473c42c0b8e00e148919d122
Potter14324 <132168110+Potter14324@users.noreply.github.com>
2024-10-22 19:37:56 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,12 @@
+const path = require('path');
 
+module.exports = {
+    entry: {
+        vendor: ['jquery', 'lodash'], // Add your libraries here
+        main: './js/main.js'            // Your main script
+    },
+    output: {
+        filename: '[name].js',          // Outputs vendor.js and main.js
+        path: path.resolve(__dirname, 'dist') // Output directory
+    }
+};
