700e75ed928e099f9275890ff441deb51cf4ac21
Potter14324 <132168110+Potter14324@users.noreply.github.com>
2024-10-22 19:34:34 +0200

    Create vendor.js

---
+++ vendor.js
@@ -0,0 +1,10 @@
+// Example content of vendor.js
+// jQuery
+(function (window, document) {
+    // jQuery code here...
+})(window, document);
+
+// Lodash
+(function () {
+    // Lodash code here...
+})();
